4ced6a74eb1f7dca4729d51af0fc1724d2093686
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-06-10 21:35:40 -0300

    Nav
    Se añadió una imagen de fondo al nav y un icono con efecto. Además, se hizo responsivo.

--- src/Componentes/NavBar/Nav.jsx
+++ src/Componentes/NavBar/Nav.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <>
-      <nav className="navbar">
+      <nav className="navbar backgroundImage">
         <div className="logo">
-          {/* <img src="logo.png" alt="Logo" /> */}
+        <Link to="/"></Link>
         </div>
-        <div className="nav-items">
+        <div className="">
           <h1>Bitácora</h1>
-          <ul>
+          <ul className='menu'>
             <li>
               <Link to="/estadisticas">Estadísticas</Link>
             </li>

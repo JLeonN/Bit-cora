ac4fe013acf3d235ba3b652384f61917db5f71ee
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-07-04 19:38:34 -0300

    Retocando el CSS

--- src/Componentes/NavBar/Nav.jsx
+++ src/Componentes/NavBar/Nav.jsx
@@ -5,20 +5,22 @@ const Nav = ({ usuarios }) => {
   return (
     <>
       <nav className="navbar backgroundImage">
-        <div className="logo"></div>
-        <div className="">
-          <h1>Bitácora</h1>
-          <ul className="menu">
-            <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to="/AgendarUsuarios">Agendar Usuarios</Link>
-            </li>
-            <li>
-              <SelectorDeUsuarios usuarios={usuarios} />
-            </li>
-          </ul>
+        <div className="contNav">
+          <div className="logo"></div>
+          <div className="">
+            <h1>Bitácora</h1>
+            <ul className="menu">
+              <li>
+                <Link to="/">Inicio</Link>
+              </li>
+              <li>
+                <Link to="/AgendarUsuarios">Agendar Usuarios</Link>
+              </li>
+              <li>
+                <SelectorDeUsuarios usuarios={usuarios} />
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
